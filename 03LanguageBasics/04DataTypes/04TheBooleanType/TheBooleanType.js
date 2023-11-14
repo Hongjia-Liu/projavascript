41226d3181a853a88a0b9b5a26f67a9055c9152f
Hongjia-Liu <hongjia_liu@outlook.com>
2023-11-14 18:41:49 +0800

    docs: the boolean type

---
+++ 03LanguageBasics/04DataTypes/04TheBooleanType/TheBooleanType.js
@@ -0,0 +1,24 @@
+let found = true;
+let lost = false;
+
+let message = "Hello world!";
+let messageAsBoolean = Boolean(message);
+
+/**
+ * flow-control statements, such as the if statement,
+ * automatically perform Boolean conversion
+ */
+if (message) {
+  console.log("Value is true");
+}
+
+// All types of values have Boolean equivalents in ECMAScript.
+console.log(Boolean("")); // false
+console.log(Boolean(+0)); // false
+console.log(Boolean(0)); // false
+console.log(Boolean(-0)); // false
+console.log(Boolean(0n)); // false
+console.log(Boolean(NaN)); // false
+console.log(Boolean(null)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean({})); // true
